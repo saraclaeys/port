661eb52d117ede85640d6ca699922b2e48979e5b
saraclaeys <sara.claeys01@gmail.com>
2020-06-08 14:27:01 +0200

    includes init.php

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,8 +1,8 @@
 <?php
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-// deze lijn aanpassen bij online plaats, de site root vragen aan je hosting
-// C:\wamp64\www\blogoop
-define('SITE_ROOT', DS . 'wamp64' . DS . 'www' . DS . 'blogoop');
+// deze lijn aanpassen bij online plaatsen, de site root vragen aan je hosting
+// C:\wamp64\www\port
+define('SITE_ROOT', DS . 'wamp64' . DS . 'www' . DS . 'port');
 defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT . DS . 'admin' . DS . 'includes');
 defined('IMAGE_PATH') ? null : define('IMAGES_PATH', SITE_ROOT . DS . 'admin' . DS . 'img');
 
@@ -14,6 +14,4 @@ require_once (INCLUDES_PATH . DS . 'Db_object.php');
 require_once (INCLUDES_PATH . DS . 'User.php');
 require_once (INCLUDES_PATH . DS . 'Photo.php');
 require_once (INCLUDES_PATH . DS . 'Session.php');
-require_once (INCLUDES_PATH . DS . 'Comment.php');
-require_once (INCLUDES_PATH . DS . 'Paginate.php');
 ?>

f44a3e222a0da2810f8564e4f157dd07f4a6bf4c
saraclaeys <sara.claeys01@gmail.com>
2020-07-07 17:29:27 +0200

    cleanup backend code

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -63,15 +63,6 @@
 
         demo.initChartist();
 
-        $.notify({
-            icon: 'pe-7s-gift',
-            message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
-
-        }, {
-            type: 'info',
-            timer: 4000
-        });
-
     });
 </script>
 

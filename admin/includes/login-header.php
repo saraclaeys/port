f3a48520b51e4eeb42587b297773e88640b5cf46
saraclaeys <sara.claeys01@gmail.com>
2020-07-08 16:46:00 +0200

    login in dashboard

--- admin/includes/login-header.php
+++ admin/includes/login-header.php
@@ -1,3 +1,6 @@
+<?php require_once ("init.php"); ?>
+<?php ob_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- admin/login.php
+++ admin/login.php
@@ -1,80 +1,103 @@
 <?php require_once("includes/login-header.php"); ?>
 
 <body>
-	
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<div class="login100-pic js-tilt" data-tilt>
-					<img src="images/login-img.jpg" alt="IMG">
-				</div>
 
-				<form class="login100-form validate-form">
+<?php
+if ($session->is_signed_in()) {
+    redirect("index.php");
+}
+
+if (isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    $user_found = User::verify_user($username, $password);
+
+    if ($user_found) {
+        $session->login($user_found);
+        redirect("index.php");
+    } else {
+        $the_message = "De gebruikersnaam of het paswoord is incorrect";
+    }
+} else {
+    $username = "";
+    $password = "";
+}
+
+?>
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="images/login-img.jpg" alt="IMG">
+            </div>
+
+            <form class="login100-form validate-form" method="post">
 					<span class="login100-form-title">
 						Dashboard Portfolio SC
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-						<input class="input100" type="text" name="email" placeholder="Email">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
+                <div class="wrap-input100 validate-input" data-validate="Voer uw gebruikersnaam in">
+                    <input class="input100" type="text" name="username" placeholder="Gebruikersnaam">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
 						</span>
-					</div>
+                </div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
-						<input class="input100" type="password" name="pass" placeholder="Password">
-						<span class="focus-input100"></span>
-						<span class="symbol-input100">
+                <div class="wrap-input100 validate-input" data-validate="Voer uw paswoord in">
+                    <input class="input100" type="password" name="password" placeholder="Paswoord">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
-					</div>
-					
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn">
-							Login
-						</button>
-					</div>
+                </div>
 
-					<div class="text-center p-t-12">
+                <div class="container-login100-form-btn">
+                    <input type="submit" name="submit" value="Login" class="login100-form-btn">
+<!--                    <button class="login100-form-btn">
+                        Login
+                    </button>-->
+                </div>
+
+                <div class="text-center p-t-12">
 						<span class="txt1">
 							Forgot
 						</span>
-						<a class="txt2" href="#">
-							Username / Password?
-						</a>
-					</div>
+                    <a class="txt2" href="#">
+                        Username / Password?
+                    </a>
+                </div>
 
-					<div class="text-center p-t-136">
-						<a class="txt2" href="#">
-							Create your Account
-							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
-						</a>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
-	
+                <div class="text-center p-t-136">
+                    <a class="txt2" href="#">
+                        Create your Account
+                        <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 
-	
-<!--===============================================================================================-->	
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
-		$('.js-tilt').tilt({
-			scale: 1.1
-		})
-	</script>
+<script src="vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script>
+    $('.js-tilt').tilt({
+        scale: 1.1
+    })
+</script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
 
 </body>
 </html>

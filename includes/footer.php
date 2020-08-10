d172e7cdca64df64174ca3217c3eb0ef08a6d67f
saraclaeys <sara.claeys01@gmail.com>
2020-08-10 10:22:22 +0200

    front-end strippen

---
+++ includes/footer.php
@@ -0,0 +1,35 @@
+<!--------------------------------------- footer contact ---------------------------------------->
+  <footer id="contact" class="contact">
+    <h2>Contact</h2>
+    <p>
+You didn't come this far to only come this far, I would like to hear from
+      you.
+    </p>
+    <ul class="social-icons no-bullets">
+      <hr />
+      <li>
+        <a href="https://www.linkedin.com/in/sara-c-160bb8199">
+          <span class="fa fa-linkedin"></span><i class="fab fa-linkedin-in"></i>
+          <span class="sr-only">LinkedIn</span>
+        </a>
+      </li>
+      <li>
+        <a href="mailto:sara.claeys90@gmail.com">
+          <span class="fa fa-envelope-o"></span>
+          <span class="sr-only">E-mail</span>
+        </a>
+      </li>
+      <hr />
+    </ul>
+  </footer>
+
+  <!--------------------------------------- scripts ---------------------------------------->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+  <script src="js/main.js"></script>
+
+  <script src="https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js"></script>
+</body>
+
+  </html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php require_once ("admin/includes/init.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Sara Claeys" />
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Sara Claeys Portfolio</title>
+</head>

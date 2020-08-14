51e80d742189e8426a57b6394cfab1847113b12d
saraclaeys <sara.claeys01@gmail.com>
2020-08-14 11:18:31 +0200

    show user

---
+++ admin/show_user.php
@@ -0,0 +1,56 @@
+<?php include("includes/header.php"); ?>
+<?php include("includes/content-top.php"); ?>
+<?php include("includes/sidebar.php"); ?>
+
+<?php
+if(empty($_GET['id'])){
+    redirect ('users.php');
+}
+
+$user = User::find_by_id ($_GET['id']);
+?>
+    <!-- partial -->
+    <div class="container-fluid">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-md-12 grid-margin">
+                <div class="d-flex align-items-baseline flex-wrap mt-3">
+                    <h2 class="mr-4 mb-0">Show user</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h3>Id</h3>
+                        <p><?php echo $user->id; ?></p>
+                        <h3>Image</h3>
+                        <?php $photo = Photo::find_by_id ($user->id); ?>
+                        <img src="../images/<?php echo $photo->name; ?>" alt="<?php echo $photo->alt; ?>" width="150px" height="auto">
+                        <p><?php echo $photo->name; ?></p>
+                        <h3>Username</h3>
+                        <p><?php echo $user->username; ?></p>
+                        <h3>Password</h3>
+                        <p><?php echo $user->password; ?></p>
+                        <h3>First name</h3>
+                        <p><?php echo $user->first_name; ?></p>
+                        <h3>Last name</h3>
+                        <p><?php echo $user->last_name; ?></p>
+                        <h3>Role</h3>
+                        <?php $role = Role::find_by_id ($user->role_id); ?>
+                        <p><?php echo $role->role; ?></p>
+                        <h3>Title</h3>
+                        <p><?php echo $user->title; ?></p>
+                        <h3>E-mail</h3>
+                        <p><?php echo $user->email; ?></p>
+                        <h3>About</h3>
+                        <p><?php echo $user->about; ?></p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- content-wrapper ends -->
+<?php include("includes/footer.php"); ?>
--- admin/users.php
+++ admin/users.php
@@ -3,68 +3,63 @@
 <?php include("includes/sidebar.php"); ?>
 
 <?php
-$users = User::find_all ();
+$users = User::find_all();
 ?>
     <!-- partial -->
-    <div class="main-panel">
-    <div class="content-wrapper">
-        <div class="row">
-            <div class="col-md-12 grid-margin">
-                <div class="d-flex align-items-baseline flex-wrap mt-3">
-                    <h2 class="mr-4 mb-0">Users</h2>
-                    <div class="d-flex align-items-baseline mt-2 mt-sm-0">
-                        <i class="fas fa-home mr-1 text-muted"></i>
-                        <i class="fas fa-chevron-right fa-xs mr-1 text-muted"></i>
-                        <p class="mb-0 mr-1">Users</p>
-                        <i class="fas fa-chevron-right fa-xs mr-1 text-muted"></i>
-                        <p class="mb-0">All users</p>
+    <div class="container-fluid">
+        <div class="content-wrapper">
+            <div class="row">
+                <div class="col-md-12 grid-margin">
+                    <div class="d-flex align-items-baseline flex-wrap mt-3">
+                        <h2 class="mr-4 mb-0">Users</h2>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <p class="card-title">All users</p>
-                        <div class="table-responsive">
-                            <table id="status-report-listing" class="table">
-                                <thead>
-                                <tr>
-                                    <th>Image</th>
-                                    <th>Username</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Role</th>
-                                    <th>Title</th>
-                                    <th>Show</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                foreach ($users as $user){
-                                    echo '<tr>';
-                                    $photo = Photo::find_by_id($user->image_id);
-                                    echo '<td><img src="../imgages/' . $photo->name . '"></td>';
-                                    echo '<td>' . $user->username . '</td>';
-                                    echo '<td>' . $user->first_name . '</td>';
-                                    echo '<td>' . $user->last_name . '</td>';
-
-                                    $role = Role::find_by_id ($user->role_id);
-                                    echo '<td>' . $role->role . '</td>';
-                                    echo '<td>' . $user->title . '</td>';
-                                    ?>
-                                    <td><a href="show_user.php?id=<?php echo $user->id; ?>" class="btn btn-danger rounded-0">show user</a></td>
+            <div class="row">
+                <div class="col-md-12 grid-margin stretch-card">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-title">All users</p>
+                            <div class="table-responsive">
+                                <table id="status-report-listing" class="table">
+                                    <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Username</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Role</th>
+                                        <th>Title</th>
+                                        <th>Show</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
                                     <?php
-                                    echo '</tr>';
-                                }
-                                ?>
+                                    foreach ($users as $user) {
+                                        echo '<tr>';
+                                        $photo = Photo::find_by_id($user->image_id);
+                                        echo '<td><img src="..images/' . $photo->name . '"></td>';
+                                        echo '<td>' . $user->username . '</td>';
+                                        echo '<td>' . $user->first_name . '</td>';
+                                        echo '<td>' . $user->last_name . '</td>';
 
-                                </tbody>
-                            </table>
+                                        $role = Role::find_by_id($user->role_id);
+                                        echo '<td>' . $role->role . '</td>';
+                                        echo '<td>' . $user->title . '</td>';
+                                        ?>
+                                        <td><a href="show_user.php?id=<?php echo $user->id; ?>"
+                                               class="btn btn-danger rounded-0">show user</a></td>
+                                        <?php
+                                        echo '</tr>';
+                                    }
+                                    ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
 
+                    </div>
                 </div>
             </div>
         </div>
